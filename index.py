3f045e93d17f40801e90c8d37d8df44e36e30ad7
Abhishek Singh <95955842+abhishekiiitbh2903@users.noreply.github.com>
2024-03-24 16:47:57 +0530

    Update index.py

--- index.py
+++ index.py
@@ -18,7 +18,7 @@ def recommend(movie):
     for i in distances[1:6]:
         # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
-        # recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
     return recommended_movie_names,recommended_movie_posters
@@ -39,20 +39,20 @@ if st.button('Show Recommendation'):
     # print(recommended_movie_posters)
     for i in range(len(recommended_movie_names)):
         st.text(recommended_movie_names[i])
-    # col1, col2, col3, col4, col5 = st.columns(5)
-    # with col1:
-    #     st.text(recommended_movie_names[0])
-    #     # st.image(recommended_movie_posters[0])
-    # with col2:
-    #     st.text(recommended_movie_names[1])
-    #     # st.image(recommended_movie_posters[1])
-    #
-    # with col3:
-    #     st.text(recommended_movie_names[2])
-    #     # st.image(recommended_movie_posters[2])
-    # with col4:
-    #     st.text(recommended_movie_names[3])
-    #     # st.image(recommended_movie_posters[3])
-    # with col5:
-    #     st.text(recommended_movie_names[4])
-    #     # st.image(recommended_movie_posters[4])
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+    
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
